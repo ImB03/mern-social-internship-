748d62751bdd0fe18a9911fa5d62dc6458a4b59f
ImB03 <giabao7803@gmail.com>
2024-02-20 12:41:33 +0700

    update

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Post from "../post/Post";
 import styles from "./posts.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useParams } from "react-router-dom";
+import { SET_POSTS } from "../../reducers/slice/slice";
 
 export default function Posts() {
   const params = useParams();
+  const dispatch = useDispatch();
+
+  console.log(params);
 
   const posts = useSelector((state) => state.persistedReducer.slice.posts);
 
   const postsSearch = useSelector(
     (state) => state.persistedReducer.search.posts
   );
+
+  useEffect(() => {
+    if (params.userId) {
+
+    }
+  }, [params]);
 
   return (
     <div className={`${styles.posts} mt-3`}>
