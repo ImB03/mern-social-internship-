9cccb81442d4bc91276abcbafb3fe30a8f75721a
ImB03 <giabao7803@gmail.com>
2024-03-08 04:56:28 +0700

    changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -21,9 +21,9 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-import postSlice from "./reducers/slice/postSlice";
-import userSlice from "./reducers/slice/userSlice";
-import searchSlice from "./reducers/slice/searchSlice";
+// import postSlice from "./reducers/slice/postSlice";
+// import userSlice from "./reducers/slice/userSlice";
+// import searchSlice from "./reducers/slice/searchSlice";
 import slice from "./reducers/slice/slice";
 import AuthSaga from "./reducers/saga/authSaga";
 import PostSaga from "./reducers/saga/postSaga";
@@ -52,9 +52,9 @@ const store = configureStore({
   reducer: {
     persistedReducer,
     // auth: authSlice,
-    post: postSlice,
-    user: userSlice,
-    search: searchSlice,
+    // post: postSlice,
+    // user: userSlice,
+    // search: searchSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
